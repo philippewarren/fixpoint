100a22bed38a8773d09cc59b1efa863d2ce78868
philippewarren <philippewarren31@gmail.com>
2021-08-20 17:46:07 -0400

    Replace product and division by bitshifts when applicable

--- fixpoint.hpp
+++ fixpoint.hpp
@@ -1,22 +1,32 @@
 #pragma once
 
+#include <limits.h>
 #include <stdint.h>
+
+namespace
+{
+    template <typename T>
+    constexpr size_t bitsizeof()
+    {
+        return sizeof(T) * CHAR_BIT;
+    };
+}
 
 class Fix
 {
 public:
     Fix() noexcept = default;
-    constexpr Fix(float value) noexcept : val(MULTIPLY_VALUE * value) {}
-    constexpr Fix(double value) noexcept : val(MULTIPLY_VALUE * value) {}
-    constexpr Fix(long double value) noexcept : val(MULTIPLY_VALUE * value) {}
-    constexpr Fix(int8_t value) noexcept : val(MULTIPLY_VALUE * value) {}
-    constexpr Fix(uint8_t value) noexcept : val(MULTIPLY_VALUE * value) {}
-    constexpr Fix(int16_t value) noexcept : val(MULTIPLY_VALUE * value) {}
-    constexpr Fix(uint16_t value) noexcept : val(MULTIPLY_VALUE * value) {}
-    constexpr Fix(int32_t value) noexcept : val(MULTIPLY_VALUE * value) {}
-    constexpr Fix(uint32_t value) noexcept : val(MULTIPLY_VALUE * value) {}
-    constexpr Fix(int64_t value) noexcept : val(MULTIPLY_VALUE * value) {}
-    constexpr Fix(uint64_t value) noexcept : val(MULTIPLY_VALUE * value) {}
+    constexpr Fix(float value) noexcept : val(value * MULTIPLY_VALUE) {}
+    constexpr Fix(double value) noexcept : val(value * MULTIPLY_VALUE) {}
+    constexpr Fix(long double value) noexcept : val(value * MULTIPLY_VALUE) {}
+    constexpr Fix(int8_t value) noexcept : val(value << BITSHIFT_VALUE) {}
+    constexpr Fix(uint8_t value) noexcept : val(value << BITSHIFT_VALUE) {}
+    constexpr Fix(int16_t value) noexcept : val(value << BITSHIFT_VALUE) {}
+    constexpr Fix(uint16_t value) noexcept : val(value << BITSHIFT_VALUE) {}
+    constexpr Fix(int32_t value) noexcept : val(value << BITSHIFT_VALUE) {}
+    constexpr Fix(uint32_t value) noexcept : val(value << BITSHIFT_VALUE) {}
+    constexpr Fix(int64_t value) noexcept : val(value << BITSHIFT_VALUE) {}
+    constexpr Fix(uint64_t value) noexcept : val(value << BITSHIFT_VALUE) {}
 
     constexpr Fix(const Fix& other) noexcept = default;
     constexpr Fix(Fix&& other) noexcept = default;
@@ -45,21 +55,21 @@ public:
 
     constexpr Fix operator*(const Fix& rhs) const noexcept
     {
-        return Fix::withValue((this->val * rhs.val) >> (sizeof(uint16_t) * 8));
+        return Fix::withValue((this->val * rhs.val) >> BITSHIFT_VALUE);
     }
     constexpr Fix& operator*=(const Fix& rhs) noexcept
     {
-        this->val = (this->val * rhs.val) >> (sizeof(uint16_t) * 8);
+        this->val = (this->val * rhs.val) >> BITSHIFT_VALUE;
         return *this;
     }
 
     constexpr Fix operator/(const Fix& rhs) const noexcept
     {
-        return Fix::withValue(((this->val << (sizeof(uint16_t) * 8)) / rhs.val));
+        return Fix::withValue(((this->val << BITSHIFT_VALUE) / rhs.val));
     }
     constexpr Fix& operator/=(const Fix& rhs) noexcept
     {
-        this->val = ((this->val << (sizeof(uint16_t) * 8)) / rhs.val);
+        this->val = ((this->val << BITSHIFT_VALUE) / rhs.val);
         return *this;
     }
 
@@ -75,19 +85,20 @@ public:
     {
         return static_cast<long double>(val) / MULTIPLY_VALUE;
     }
-    constexpr explicit operator int8_t() noexcept { return val / MULTIPLY_VALUE; }
-    constexpr explicit operator uint8_t() noexcept { return val / MULTIPLY_VALUE; }
-    constexpr explicit operator int16_t() noexcept { return val / MULTIPLY_VALUE; }
-    constexpr explicit operator uint16_t() noexcept { return val / MULTIPLY_VALUE; }
-    constexpr explicit operator int32_t() noexcept { return val / MULTIPLY_VALUE; }
-    constexpr explicit operator uint32_t() noexcept { return val / MULTIPLY_VALUE; }
-    constexpr explicit operator int64_t() noexcept { return val / MULTIPLY_VALUE; }
-    constexpr explicit operator uint64_t() noexcept { return val / MULTIPLY_VALUE; }
+    constexpr explicit operator int8_t() noexcept { return val >> BITSHIFT_VALUE; }
+    constexpr explicit operator uint8_t() noexcept { return val >> BITSHIFT_VALUE; }
+    constexpr explicit operator int16_t() noexcept { return val >> BITSHIFT_VALUE; }
+    constexpr explicit operator uint16_t() noexcept { return val >> BITSHIFT_VALUE; }
+    constexpr explicit operator int32_t() noexcept { return val >> BITSHIFT_VALUE; }
+    constexpr explicit operator uint32_t() noexcept { return val >> BITSHIFT_VALUE; }
+    constexpr explicit operator int64_t() noexcept { return val >> BITSHIFT_VALUE; }
+    constexpr explicit operator uint64_t() noexcept { return val >> BITSHIFT_VALUE; }
 
 private:
     int64_t val;
 
     constexpr static uint64_t MULTIPLY_VALUE = UINT16_MAX;
+    constexpr static uint64_t BITSHIFT_VALUE = bitsizeof<uint16_t>();
 
     constexpr static Fix withValue(int64_t value) noexcept
     {
